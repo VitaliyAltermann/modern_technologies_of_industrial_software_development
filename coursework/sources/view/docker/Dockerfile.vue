0895c684a67f772f5218104aeabd0be67108e2d4
Vitaliy Altermann <vitaliy.altermann@gmail.com>
2023-11-11 21:44:45 +0300

    Поправил параметры сборки контроллера

---
+++ coursework/sources/view/docker/Dockerfile.vue
@@ -0,0 +1,13 @@
+FROM node:19-alpine as build-stage
+WORKDIR /app
+COPY ./package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+# этап production (production-stage)
+FROM nginx:1.24.0-alpine-slim as production-stage
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+EXPOSE 80 8080
+CMD ["nginx", "-g", "daemon off;"]
